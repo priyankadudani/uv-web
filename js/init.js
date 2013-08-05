97fe6271f492c7669ebc2f618456e7808da8de27
Priyanka Dudani <dudani.priyanka@gmail.com>
2013-08-05 16:57:48 +0530

    Updated js. making it simpler

--- js/init.js
+++ js/init.js
@@ -1,60 +1,63 @@
 $(function () {
 	var space = $('#uv-graph-space');
-		var horConfig = $.extend(true, {}, uv.config);
-		var verConfig = $.extend(true, {}, uv.config);
-		
-		horConfig.dimension = {
-				width : 500,
-				height : 300
-		};
-		
-		horConfig.margin = {
+	
+	var horConfig = {
+		dimension : {width : 500,
+			height : 300
+		},
+		margin : {
 				top : 40,
 				bottom : 60,
 				left : 100,
 				right : 100
-		};
-		
-		verConfig.dimension = {
-				width : 500,
-				height : 300
-		};
-		
-		verConfig.margin = {
+		},
+		meta : {
+			position : '#uv-graph-space',
+			isDownload : false
+		}
+	};
+	
+	var verConfig = {
+		graph : {
+			orientation : 'Vertical'
+		},
+		dimension : {width : 500,
+			height : 300
+		},
+		margin : {
 				top : 40,
 				bottom : 60,
 				left : 100,
 				right : 100
-		};
-		
-		horConfig.meta.position = '#uv-graph-space';
-		horConfig.meta.isDownload = false;
-		
-		verConfig.meta.position = '#uv-graph-space';
-		verConfig.meta.isDownload = false;
-		
-		verConfig.graph.orientation = 'Vertical';
+		},
+		meta : {
+			position : '#uv-graph-space',
+			isDownload : false
+		}
+	};
 	
-		barHorTest = uv.chart('Bar', new uv.Test().getGraphDef(), horConfig);
-		verConfig.graph.palette = 'Lemon';
-		barVerTest = uv.chart('Bar', new uv.Test().getGraphDef(), verConfig);
-		
-		verConfig.graph.palette = 'Candid';
-		stepUpBarHorTest = uv.chart('StepUpBar', new uv.Test().getGraphDef(), verConfig);
-		
-		verConfig.graph.palette = 'New Moon';
-		stackedBarHorTest = uv.chart('StackedBar', new uv.Test().getGraphDef(), verConfig);
+	uv.chart('Bar', new uv.Test().getGraphDef(), horConfig);
 	
-		lineHorTest = uv.chart('Line', new uv.Test().getGraphDef(), horConfig);
+	verConfig.graph.palette = 'Lemon';
+	uv.chart('Bar', new uv.Test().getGraphDef(), verConfig);
 	
-		areaHorTest = uv.chart('Area', new uv.Test().getGraphDef(), horConfig);
+	verConfig.graph.palette = 'Candid';
+	uv.chart('StepUpBar', new uv.Test().getGraphDef(), verConfig);
 	
-		stackareaHorTest = uv.chart('StackedArea', new uv.Test().getGraphDef(), horConfig);
-		
-		centareaVerTest = uv.chart('PercentArea', new uv.Test().getGraphDef(), verConfig);
-		
-		centbarVerTest = uv.chart('PercentBar', new uv.Test().getGraphDef(), verConfig);
-		
-		donutTest = uv.chart('Donut', new uv.Test().getGraphDef(), horConfig);
-		pieTest = uv.chart('Pie', new uv.Test().getGraphDef(), horConfig);
+	verConfig.graph.palette = 'New Moon';
+	uv.chart('StackedBar', new uv.Test().getGraphDef(), verConfig);
+
+	uv.chart('Line', new uv.Test().getGraphDef(), horConfig);
+
+	uv.chart('Area', new uv.Test().getGraphDef(), horConfig);
+
+	uv.chart('StackedArea', new uv.Test().getGraphDef(), horConfig);
+	
+	uv.chart('PercentArea', new uv.Test().getGraphDef(), verConfig);
+	
+	uv.chart('PercentBar', new uv.Test().getGraphDef(), verConfig);
+	
+	uv.chart('Donut', new uv.Test().getGraphDef(), horConfig);
+	
+	uv.chart('Pie', new uv.Test().getGraphDef(), horConfig);
 });
